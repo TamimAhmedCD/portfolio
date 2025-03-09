1645f3c6e048ffe22bd2d4e3dc2d94625bf51dfa
Tamim Ahmed <tamim20072@gmail.com>
2025-03-09 19:10:46 +0600

    project json structure updated

--- components/Work/Work.jsx
+++ components/Work/Work.jsx
@@ -3,17 +3,18 @@ import { Timeline } from "../ui/timeline";
 import projects from "../../public/projects.json";
 
 export function Work() {
-  const data = projects.projects.map((project) => ({
+  console.log(projects);
+  const data = projects.map((project) => ({
     title: project.name,
     tech: "Tech Used:",
-    techIcons: project.techIcons.map((icon, index) => (
-      <span key={index} dangerouslySetInnerHTML={{ __html: icon }} />
+    techIcons: project.techStack.map((tech, index) => (
+      <img key={index} src={tech.logo} alt={tech.name} className="h-6 w-6 inline-block mr-2" />
     )),
-    desc: project.desc,
+    desc: project.description, // Fix: Use "description" instead of "desc"
     content: (
       <div>
         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          {project.shortDesc}
+          {project.shortDescription} {/* Fix: Use "shortDescription" instead of "shortDesc" */}
         </p>
         <div className="grid grid-cols-2 gap-4">
           {project.images.map((img, index) => (
@@ -30,6 +31,7 @@ export function Work() {
       </div>
     ),
   }));
+  
 
   return (
     <div className="">
